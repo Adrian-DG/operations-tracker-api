51ae50417d14252dc8be84ba8a599417bc41d130
Adrian-DG <adrian96guilamo@gmail.com>
2025-02-10 13:44:03 -0400

    feat: add @nestjs/config for environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { environmentConfig } from './helpers/environment.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(environmentConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
