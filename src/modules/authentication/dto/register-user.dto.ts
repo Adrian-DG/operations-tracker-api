3345194836f78ada73e9c2606d5dac8cbaa7773d
Adrian-DG <adrian96guilamo@gmail.com>
2025-02-10 15:56:01 -0400

    feat: add Swagger API properties to RegisterUserDto for improved documentation

--- src/modules/authentication/dto/register-user.dto.ts
+++ src/modules/authentication/dto/register-user.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RegisterUserDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly username: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly password: string;
 }
