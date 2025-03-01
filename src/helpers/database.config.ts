a4b7a40c43199cb459a56566ea34048a72f13d99
Adrian-DG <adrian96guilamo@gmail.com>
2025-03-01 17:59:36 -0400

    feat: add mssql dependency and update database configuration for potential SQL Server integration

--- src/helpers/database.config.ts
+++ src/helpers/database.config.ts
@@ -1,6 +1,27 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 
+// const sqlServerProductionConfig: TypeOrmModuleOptions = {
+//   type: 'mssql',
+//   host: 'tcp:asistenciavialdbserver.database.windows.net',
+//   authentication: {
+//     type: 'default',
+//     options: { userName: 'SqlAdmin', password: 'Mopc8044' },
+//   },
+//   username: 'SqlAdmin',
+//   password: 'Mopc8044',
+//   database: 'asistenciaVialDb',
+//   options: { encrypt: true, trustServerCertificate: false },
+// };
+
+// const sqlServerDevelopmentConfig: TypeOrmModuleOptions = {
+//   type: 'mssql',
+//   host: 'localhost',
+//   port: 1433,
+//   options: { trustServerCertificate: true },
+//   database: 'Operations_Tracker_DB',
+// };
+
 const sqliteConfig: SqliteConnectionOptions = {
   type: 'sqlite',
   database: '.db/operations.db',
@@ -8,8 +29,8 @@ const sqliteConfig: SqliteConnectionOptions = {
 
 export const DATABASE_CONFIG: TypeOrmModuleOptions = {
   ...sqliteConfig,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: false,
+  // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
   autoLoadEntities: true,
   retryAttempts: 3,
   retryDelay: 3000,
