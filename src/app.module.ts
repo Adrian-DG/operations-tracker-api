943bb73823f12e660991d5d034321fc6450cb047
Adrian-DG <adrian96guilamo@gmail.com>
2025-04-23 13:33:43 -0400

    feat: add permissions for activity subtypes in Permissions enum

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DATABASE_CONFIG } from './helpers/database.config';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
